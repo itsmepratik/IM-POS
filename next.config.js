8907f0723a2cfbc165568c382d8316b7a0d14de7
itsmepratik <pratikckb@gmail.com>
2025-03-25 00:31:30 +0400

    16th - fixed deployment issue

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
   experimental: {
     forceSwcTransforms: true,
   },
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  // Add basePath configuration if deploying to a subdirectory
+  // basePath: '',
 };
 
 module.exports = nextConfig; 
