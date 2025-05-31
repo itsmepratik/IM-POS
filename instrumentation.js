73ffe390e3aaac026b7e7bb70f8b30ab51facdcd
itsmepratik <pratikckb@gmail.com>
2025-06-01 00:24:41 +0400

    44th commit

---
+++ instrumentation.js
@@ -0,0 +1,6 @@
+// This file is required by Next.js for instrumentation
+// It's automatically recognized and used
+export function register() {
+  // This is intentionally left empty
+  // The presence of this file resolves the module not found error
+}
