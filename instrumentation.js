40ed1345a26b20c8552f87a354d599da2e2b99e7
itsmepratik <pratikckb@gmail.com>
2026-02-24 23:52:38 +0400

    Root level updates

--- instrumentation.js
+++
@@ -1,6 +0,0 @@
-// This file is required by Next.js for instrumentation
-// It's automatically recognized and used
-export function register() {
-  // This is intentionally left empty
-  // The presence of this file resolves the module not found error
-}
